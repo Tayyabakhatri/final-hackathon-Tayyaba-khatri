76b82c275f36c3943b3ea6ab9dcd1b44f94b744b
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-04-17 09:16:32 +0500

    modified

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import "dotenv/config"
-const url = process.env.MONGODB_URL
-console.log(url);
+// const url = process.env.MONGODB_URL
+// console.log(url);
 import chalk from "chalk";
 const connectedToDb = async () => {
     // mongoose.connection.on("connected", () => console.log("✅ DB Connected"));
 
     try {
-        await mongoose.connect(url, { dbname: "finalHackathonDB" });
+        await mongoose.connect(process.env.MONGODB_URL, { dbname: "finalHackathonDB" });
         console.log(chalk.bgBlue.white("✅ Connected to MongoDB finalHackathonDB"));
     } catch (error) {
         console.error(chalk.bgRed.white("❌ Error connecting to MongoDB:"), error.message);
