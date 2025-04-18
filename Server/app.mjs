5a752a7d64c7b0697608144ea48f77f3f9df4f5b
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-04-18 09:36:35 +0500

    ...

--- Server/app.mjs
+++ Server/app.mjs
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000
 
 
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:5174'],
+    origin:['http://localhost:5173','http://localhost:5174',"final-hackathon-tayyaba-khatri-production.up.railway.app"],
     methods:["GET","PUT",'POST',"DELETE"],
     credentials: true,
     allowedHeaders:["Content-Type","Authorization"]
