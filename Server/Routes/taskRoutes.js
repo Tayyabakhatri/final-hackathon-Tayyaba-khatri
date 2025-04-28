b40361f3e70159ef32275cc1c9675d9ffab57931
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-04-28 16:58:13 +0500

    modified

--- Server/Routes/taskRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { createTask, getAllTasks,updateTask, deleteTask } from '../Controller/taskController.js';
-router.post('/task', createTask)
-router.get('/taskss', getAllTasks)
-router.put('/task/:id', updateTask)
-router.delete('/task/:id', deleteTask)
-
-export default router;
